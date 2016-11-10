e40af8146df94c7e007809c50bbe15cb8ead2a5f
ayanna92 <Ayanna@Ayannas-MacBook-Air.local>
2016-11-10 14:49:38 +0100

    juiste files

---
+++ studentenenvakken.py
@@ -0,0 +1,14 @@
+import csv
+with open('studentenenvakken.csv') as csvfile:
+    studenten = csv.DictReader(csvfile)
+    for row in studenten:
+        # door column namen uit csv door te geven als row['naam'] kun je
+        # onderdelen die je interesseren uit csv file selecteren
+        print(row['Achternaam'], row['Voornaam'], row['Vak1'])
+
+import csv
+with open('zalen.csv', 'rU') as csvfile:
+    zalen = csv.DictReader(csvfile)
+    for row in zalen:
+        print row['zaalnummer'], row['max_capaciteit']
+hello    
