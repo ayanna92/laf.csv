be0dd644fc249b80f5becba128819d591b1878f8
10533176 <fvanson@live.nl>
2016-11-11 15:34:31 +0100

    import of the courses

--- classVakken.py
+++ classVakken.py
@@ -1,17 +1,39 @@
-# vakken classes
+# class for courses
 # Lectures & roosters
 #
 # Ayanna, Lois, Femke
 #
 
-import studentenvakken.py
+import csv
+fil_2 = open('vakken2.csv')
+file_vakken = csv.reader(fil_2)
 
-class vakken(object):
-        """
-        descriptives for each course
-        """
+courses = []
+courses_info = dict()
 
-        def __init__(self):
+# read csv file in variable courses
+for rows in file_vakken:
+    courses.append(rows)
 
-            self.name = vakken['vakken']
-            print self.name
+# put al the courses names in variable names
+for col in courses[0:]:
+    course_name = col[0]
+
+# combine the names and the activities of the courses
+    if col[1] != '0':
+        course_activity= ': hoorcollege '
+
+    if col[2] == '1':
+        course_activity += '+ werkgroep '
+
+    if col[4] == '1':
+        course_activity += '+ practica'
+
+    # add new line to make it readable
+    course_activity + '\n'
+
+    # combine names and activities
+    name_and_activity = course_name + course_activity
+    course_activity = ""
+
+    #print name_and_activity
