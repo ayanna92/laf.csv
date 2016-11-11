8f36e6c3a4ea8414a45801ede0eaae3d74b2fe58
ayanna92 <Ayanna@wcw-stud-145-109-0-116.wireless.uva.nl>
2016-11-11 15:17:25 +0100

    class students
    student object en informatie uit csv voor student (welke vakken en
    studentnummers)

--- class_students.py
+++ class_students.py
@@ -1,34 +1,52 @@
-# students
-# Lectures & roosters
-#
-# Ayanna, Femke & Lois
-#
-
-import csv
-
 class students(object):
-    with open('studentenenvakken.csv') as csvfile:
-        studenten = csv.DictReader(csvfile)
-        info = []
+    """
+    Object that will contain student number, student courses
+    and number of courses.
+    """
 
-        for row in studenten:
-        # door column namen uit csv door te geven als row['naam'] kun je
-        # onderdelen die je interesseren uit csv file selecteren
-            info.append(row)
-        print info
+    def __init__(self, stud_num, stud_vak):
 
+        self.stud_num = stud_num
+        self.stud_vak = stud_vak
 
+    def vakNumber(self):
 
+        return len(self.stud_vak)
 
+##########################
+import csv
+# open and read csv files
+open_1 = open('studentenenvakken.csv')
+file_1 = csv.reader(open_1)
 
+# make empty list for student information, and list which will contain info
+stud_info = []
+stud_list = []
 
+# fills empty list with student information
+for row in file_1:
+    stud_info.append(row)
 
-#class students(object):
+# for every column in student info
+for col in stud_info[1:]:
 
-    #def __init__(self):
-        # voor/achternaam (studentnummer)
-        #for row in
+    # student number ( in column 2)
+    stud_num = col[2]
 
-        # aantal vakken [1-5]
+      # make empty list for courses and fill list with rows from
+      # column 3 - end
+      # don't add if column is empty ("") and fill list
+    stud_vak = []
+    for vak in col[3:]:
+        if vak != "":
+            stud_vak.append(vak)
 
-        # naam vak [max array of 5]
+    #check
+    print stud_vak
+
+      # add student object to list
+    student = students(stud_num, stud_vak)
+    stud_list.append(student)
+
+#check
+print stud_info
