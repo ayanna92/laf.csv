272bd49326e7e798a92a2233b1ad72ae5844123e
loisie123 <lois.vanvliet@student.uva.nl>
2016-11-11 15:46:23 +0100

    beetje veranderd

---
+++ activiteit.py
@@ -0,0 +1,35 @@
+# Classe activiteit
+
+
+# het berekenen van het aantal hoorcolleges, werkcolleges en practica per vak.
+# door het gebruik van studenten.py het berekenen van de capaciteit van elk college
+import class_students.py
+
+
+class activiteit (object):
+    """
+    object that wil contain how many hoorcollege, werkcolleges and practica
+    are needed for each course.
+    """
+    def __init__(self, stud_vak):
+        self.stud_vak = stud_vak
+
+    def hoorcollege():
+        """
+        returns how many hoorcollege the course has
+        Dit staat in het vakken csv
+        """
+
+    def werkcolleges():
+        """
+        returns how many werkcollege the course has
+
+        Hier moet rekening gehouden met hoeveel studenten er zijn en daaruit moeten dus het aantal colleges gehaald worden.
+        """
+
+    def practica():
+        """
+        returns how many practica the course has
+
+        Hier moet rekening gehouden met hoeveel studenten er zijn en daaruit moeten dus het aantal pracica gehaald worden.
+        """
