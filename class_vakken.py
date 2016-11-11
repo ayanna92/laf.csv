e75b5470bde5c3727488e7702ca534c89923b424
10533176 <fvanson@live.nl>
2016-11-11 16:03:40 +0100

    final class_vakken

--- class_vakken.py
+++ class_vakken.py
@@ -4,12 +4,25 @@
 # Ayanna, Lois, Femke
 #
 
+class courses(object):
+    """
+    Object that will contain course name and course name and activities
+    """
+
+    def __init__(self, name, name_and_activity):
+        """
+        Initialize student and corresponding courses
+        """
+
+        self.course_name = course_name
+        self.name_and_activity = name_and_activity
+
+
 import csv
 fil_2 = open('vakken2.csv')
 file_vakken = csv.reader(fil_2)
 
 courses = []
-courses_info = dict()
 
 # read csv file in variable courses
 for rows in file_vakken:
@@ -17,23 +30,23 @@ for rows in file_vakken:
 
 # put al the courses names in variable names
 for col in courses[0:]:
-    course_name = col[0]
+    name = col[0]
 
 # combine the names and the activities of the courses
     if col[1] != '0':
         course_activity= ': hoorcollege '
 
     if col[2] == '1':
-        course_activity += '+ werkgroep '
+        course_activity += 'werkgroep '
 
     if col[4] == '1':
-        course_activity += '+ practica'
+        course_activity += 'practicum'
 
     # add new line to make it readable
     course_activity + '\n'
 
     # combine names and activities
-    name_and_activity = course_name + course_activity
+    name_and_activity = name + course_activity
     course_activity = ""
 
     #print name_and_activity
