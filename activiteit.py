18aa9332f58b70b7d19214003471ffe5dfea2182
loisie123 <lois.vanvliet@student.uva.nl>
2016-11-14 23:40:43 +0100

    class activiteit finished

--- activiteit.py
+++ activiteit.py
@@ -4,6 +4,43 @@
 
 import class_students
 import class_vakken
+import math
+from collections import Counter
+import csv
+
+# open and read csv files
+open_1 = open('studentenenvakken.csv')
+file_1 = csv.reader(open_1)
+
+fil_2 = open('vakken2.csv')
+file_vakken = csv.reader(fil_2)
+#skip header
+file_1.next()
+
+# make list for how many students are taking each course
+vak = []
+
+for row in file_1:
+    vak.append(row[3])
+    vak.append(row[4])
+    vak.append(row[5])
+    vak.append(row[6])
+    vak.append(row[7])
+
+# example
+name_course = "Calculus 2"
+number_of_students = vak.count(name_course)
+#print vak
+#print len(vak)
+#print Counter(vak)
+#for row in file_vakken:
+#    if name_course in row and row[2] != 0:
+#        capacity = float(row[3])
+
+#print number_of_students
+#print capacity
+#print math.ceil(number_of_students / capacity)
+
 
 class activiteit (object):
     """
@@ -14,7 +51,7 @@ class activiteit (object):
         self.name_course = name_course
         self.stud_num = stud_num
         self.name_and_activity = name_and_activity
-
+        self.number_of_students = vak.count(name_course)
 
     def hoorcollege(name_course):
         """
@@ -24,20 +61,38 @@ class activiteit (object):
 
         for row in file_vakken:
             if name_course in row:
-                return row[2]
+                return row[1]
 
-    def werkcolleges():
+    def werkcolleges(number_of_students, name_course):
         """
         returns how many werkcollege the course has
         Er is nooit meer dan 1 werkgroep
         Hier moet rekening gehouden met hoeveel studenten er zijn en daaruit moeten dus het aantal colleges gehaald worden.
-        """
+        """"
+        for row in file_vakken:
+            if name_course in row and row[2] != 0:
+                capacity = float(row[3])
+
+        #print number_of_students
+        #print capacity
+        return math.ceil(number_of_students / capacity)  # klopt nog niet helemaal want geeft nog een verkeerd afgerond getal terug.
+
 
     def practica():
         """
+
         returns how many practica the course has ER is ook altijd maar 1 practica per week
         Hier moet rekening gehouden met hoeveel studenten er zijn en daaruit moeten dus het aantal pracica gehaald worden.
         """
+
+        for row in file_vakken:
+            if name_course in row and row[5] != 0:
+                capacity = float(row[6])
+
+        #print number_of_students
+        #print capacity
+        return math.ceil(number_of_students / capacity)
+
 
 ################
 # make courses and calculate their capacity
@@ -45,57 +100,14 @@ class activiteit (object):
 
 
 
-from collections import defaultdict, Counter
-data = defaultdict(list)
 
-student_vakken.next() #skip header
 
-vak1 = [row[3] for row in student_vakken]
-print len(vak1)
-vak2 = [row[4] for row in student_vakken]
-vak3 = [row[5] for row in student_vakken]
-vak4 = [row[6] for row in student_vakken]
-vak5 = [row[7] for row in student_vakken]
+
+"""
 
 
 
 #from collections import defaultdict, Counter
 #for (k,v) in Counter(stud_vak).dataitems():
 #    print "%s appears %d studenten" % (k, v)
-
-
-
-
-
-
-
-
-
-#courses = []
-#courses_list = []
-#data = defaultdict(list)
-
-# read csv file in variable courses
-#for rows in file_vakken:
-    #courses.append(rows)
-
-# put al the courses names in variable names
-#for col in courses[0:]:
-#    name = col[0]
-    # for name in data.items()
-#        number_of_students = count(name)
-#    print name
-#    print number_of_students
-
-
-# combine names and activit
-
-
-# een forloop maken waarbij een tabel wordt gemaakt met daarin de benodigde informatie
-# voor elke vak in vakken.csv:
-#   schrijf de naam
-#   check hoeveel keer de naam voorkomt in studenten.csv
-#   Maak een nieuwe tabel aan waar deze waardes in staan.
-#for x in range (0 , len(file_vakken)):
-    #name = file_vakken[x]
-    #number_of_students = student_vakken.count(name)
+"""
