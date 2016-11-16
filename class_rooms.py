c44d07473a26a890507d86472e770c451820cd47
ayanna92 <Ayanna@wcw-stud-145-109-3-40.wireless.uva.nl>
2016-11-16 11:59:55 +0100

    .

--- class_rooms.py
+++ class_rooms.py
@@ -20,28 +20,3 @@ class Classrooms(object):
 
         # example of filling in the timeslots
         # tijdsloten['9.00'] = activiteit
-
-
-###################################
-
-import csv
-
-fil = open('classrooms.csv')
-file_zalen = csv.reader(fil)
-
-zalen = []
-classroom_list = []
-
-
-# put csv file in variable zalen
-for rows in file_zalen:
-    zalen.append(rows)
-
-# put al the room numbers in room
-for col in zalen[1:]:
-    room = col[0]
-    capacity = col[1]
-
-    # add classroom object to list
-    classroom = Classrooms(room, capacity)
-    classroom_list.append(classroom)
