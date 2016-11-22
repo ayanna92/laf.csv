c7c73f9628ee50024de5c82c999d424b35784140
loisie123 <lois.vanvliet@student.uva.nl>
2016-11-22 14:49:52 +0100

    class rooster + class tijdslot aangemaakt

---
+++ class_tijdslot.py
@@ -0,0 +1,34 @@
+
+
+
+class TijdSlot(object):
+    """
+    representeerd een tijdsblok
+    """
+    def __init__(self):
+
+        self.course = Courses.name_course
+        self.student = Students.stud_num
+
+
+    def emptyTijdslotRoom(self):
+        """
+        om te checken of er in het tijdslot al een classroom is ingecheckt
+        returns true or false
+        """
+
+
+
+
+    def emtpyTijdslotStudent(self):
+        """
+        om te checken of er in het tijdslot al een student is ingeboekt
+        returns true or false
+
+        """
+
+    def fillSlot(self):
+        """
+        Vult tijdslot
+        
+        """
