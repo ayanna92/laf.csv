f5a735b07647a8dc92012468c757f77c647e1070
loisie123 <lois.vanvliet@student.uva.nl>
2016-12-04 15:26:29 +0100

    scoringsfunctie op basis van oude rooster is af. Nu nog aanpassen op het nieuwe rooster

--- scoringsfunctie.py
+++ scoringsfunctie.py
@@ -16,6 +16,12 @@ file_zalen = csv.reader(fil_3)
 zalen = []
 course_student_list = csv.reader(open('course_stud_num.csv'))
 course_and_student = []
+student_course_list = csv.reader(open('studentenenvakken.csv'))
+student_en_hun_vakken = []
+
+next(student_course_list)
+for row in student_course_list:
+    student_en_hun_vakken.append(row)
 
 for row in file_zalen:
     zalen.append(row)
@@ -35,8 +41,8 @@ tijdsblok = 0
 tijdsblokken = []
 count  = 0
 capaciteit_zaal = []
+points = 10000
 minpunten = 0
-
 
 # voor elk vak een rooster maken wanneer ze voorkomen.
 for vak in vakken:
@@ -51,36 +57,50 @@ for vak in vakken:
            #print capacity_vak
     for rooms, rooster_per_room in rooster.items():
         zaal = 0
-
+        #print rooms
         capaciteit_zaal = 0
         # wat is de capaciteit van de zaal:
         for meerzalen in zalen:
             if rooms == meerzalen[0]:
                 capaciteit_zaal = int(meerzalen[1])
-                #print "capaciteit_zaal:"
-                #print capaciteit_zaal
 
 
         # selecteer voor elke zaal een dag.
         for zaal_per_dag in rooster_per_room:
-            for tijdsblok_per_zaal in zaal_per_dag:
-                #print tijdsblok_per_zaal
+            for vak_per_zaal in zaal_per_dag:
+                #print vak, "hoi"
+                #print vak_per_zaal
                 # check of het vak in het tijdsblok zit.
-                if vak == tijdsblok_per_zaal:
+                #if vak == vak_per_zaal:
+                if vak in vak_per_zaal:
+
+                    #if "werkcollege" in vak_per_zaal:
+                    #    for row in file_vakken:
+                    #        if vak == row[0]:
+                    #            capacity_vak = float(row[3])
+
+
+                    #if "hoorcollege" in vak_per_zaal:
+                        #for students in course_and_student:
+                        #    if students[0] == vak:
+                        #       student_list = students[1:]
+                        #       vak_name = Courses(vak, student_list)
+                        #       capacity_vak = vak_name.studNumber()
+                               #print "capacity:"
+                               #print capacity_vak
+                    #if "pracica" in vak_per_zaal:
+                        #    for row in file_vakken:
+                        #        if vak == row[0]:
+                        #            capacity_vak = float(row[5])
                     # voeg toe welke plaats tot het rooster.
                     tijdsblokken.append((dag,tijdsblok))
                     #print tijdsblokken
+                    #print "capaciteit van het vak", capacity_vak
+                    #print "capaciteit van de zaal", capaciteit_zaal
                     # dan zit het vak dus in deze specifieke zaal
-                    print capacity_vak
-                    print capaciteit_zaal
-
                     if capacity_vak > capaciteit_zaal:
-                        print "vak past niet in de zaal"
+                        #print "vak past niet in de zaal"
                         minpunten += capacity_vak - capaciteit_zaal
-                    elif (capacity_vak) < (capaciteit_zaal):
-                        print "vak past er in"
-
-
                 tijdsblok += 1
             tijdsblok = 0
             dag = dag +1
@@ -88,14 +108,15 @@ for vak in vakken:
 
     vakrooster[vak] = tijdsblokken
     tijdsblokken = []
-
-print minpunten
+points -= minpunten
+print "minpunten vanwege dat het vak niet in de zaal past:" ,minpunten
 
 
 #print vakrooster
 
 # als een vak op eenzelfde uur wordt gegeven:
 #for vak in vakrooster.keys():
+#print vakrooster
 maluspunten = 0
 for vak in vakrooster.values():
     for tijdsblok in vak:
@@ -104,7 +125,55 @@ for vak in vakrooster.values():
             maluspunten += 10
         if vak.count(tijdsblok) == 3:
             maluspunten += (20/3)
-print maluspunten
+print "punten vanwege vakken tegelijkertijd ingeroosterd:",maluspunten
+
+
+
+
+
+
+
+
+
+
+
+
+studentrooster = {}
+
+## aanpassingen zijn nodig!
+for student in student_en_hun_vakken:
+    vak_rooster_student = []
+    for vakken in student[3:7]:
+        if vakken != "":
+            rooster_vak = vakrooster[vakken]
+            #print rooster_vak
+            vak_rooster_student.append(rooster_vak)
+    #print vak_rooster_student
+
+    studentrooster[student[2]] = vak_rooster_student
+
+#print studentrooster
+minpuntstudent = 0
+for student in studentrooster.values():
+    #studentpunt = 0
+    for vakblok in student:
+        for tijdsblok in vakblok:
+            amount = 0
+            for andere_vakblok in student:
+                amount += andere_vakblok.count(tijdsblok)
+                #print tijdsblok, "   in    ", andere_vakblok ,  amount, "keer   "
+            if amount >= 2:
+                #studentpunt += float(1.0/amount)
+                #print studentpunt
+                #print "true"
+                minpuntstudent += float(1.0/amount)
+                # hij gaat amount aantal keer een bepaalde waarde checken.
+
+
+
+
+print "punten voor studenten die conflicten hebben:", minpuntstudent
+                #print amount
 
 
 
@@ -133,6 +202,7 @@ def scoringsfunctie(schedulerooms_list, vakken):
 # bonuspunten:
 # als een vak x aantal activiteit heeft en deze ook op x dagen zijn ingeroostred
     # points += 20
+
 #minpunt:
 # als een vak x aantal activieit heeft en deze op x-1 dagen zijn ingeroosterd
     # points -= 10
