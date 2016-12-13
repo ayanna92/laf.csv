8b17c3b31843d6c74a8e8c4629cfb7a4b95344c8
10533176 <fvanson@live.nl>
2016-12-13 21:42:39 +0100

    hill climber

--- hill_climbing.py
+++ hill_climbing.py
@@ -44,36 +44,26 @@ def hill_climbing():
         hour = 0
         #print schedule
         for key, week in list(schedule.items()):
-            print "key':",  key
-            print "week:", week
+
             for days in week:
-                print days
+
                 for hours in days:
+                    print "start:"
+                    print key
+                    print days
                     print hours
+
                     for row in course_and_activity:
+
                         course = row
                         # remember current course in schedule
                         current_course = schedule[key][day][hour]
-
-
-                        #remember the score of the schedule without new value
-                        scoring_schedule_previous = scoringsfunctie(schedule, courses)
-
-                        print "course:", course
-                        current_course = schedule[key][day][hour]
-                        print "schedule" ,schedule[key][day][hour]
-                        if current_course in course_and_activity:
-                            current_course = current_course
-                        else:
-                            current_course = row[0]
-
-                        schedule[key][day][hour] = course
-                        print " new schedule:", schedule
-
-                        # only remember current course when it is still in course_and_activity list
-
                         # put in new value in schedule
                         schedule[key][day][hour] = course
+
+                        #find value of course in dictionary
+                        # fill in current_course op de plek waar course nu staat
+
 
                         # score the new value
                         scoring_schedule_current = scoringsfunctie(schedule, courses)
@@ -85,47 +75,10 @@ def hill_climbing():
                             print 'current highscore', high_score
                             print 'met course', course
                             keep_track_new_course = course
-
-                        if scoring_schedule_previous < scoring_schedule_current:
-                            keep_track_secondbest_score = course
-
-                        if scoring_schedule_previous > scoring_schedule_current:
-                            if current_course in course_and_activity:
-                                previous_best_score = current_course
-                            elif keep_track_secondbest_score != '':
-                                if keep_track_secondbest_score in course_and_activity:
-                                    previous_best_score = keep_track_secondbest_score
-                            else:
-                                previous_best_score = course
                         else:
-                            previous_best_score = course
+                            keep_track_new_course = current_course
 
-                    print "invullen beste vak... "
-                    print "met highscore: ", high_score
-                    if keep_track_new_course != 'empty':
-                        schedule[key][day][hour] = keep_track_new_course
-                        course_and_activity.remove(keep_track_new_course)
-                        print 'new course:', keep_track_new_course
-                    elif keep_track_secondbest_score != 'empty':
-                        schedule[key][day][hour] = keep_track_secondbest_score
-                        course_and_activity.remove(keep_track_secondbest_score)
-                        print 'second best', keep_track_secondbest_score
-                    elif previous_best_score != 'empty':
-                        schedule[key][day][hour] = previous_best_score
-                        course_and_activity.remove(previous_best_score)
-                        print 'vorige vak was beter', previous_best_score
-                    else:
-                        schedule[key][day][hour] = course
-                        course_and_activity.remove(course)
-                        print 'geen van de opties', course
-
-                    print schedule
-                    keep_track_new_course = 'empty'
-                    keep_track_secondbest_score = 'empty'
-                    previous_best_score = 'empty'
-                    schedule = schedule
-
-                        schedule[key][day][hour] = keep_track_new_course
+                    schedule[key][day][hour] = keep_track_new_course
 
                     print 'current schedule'
                     print schedule
@@ -138,8 +91,7 @@ def hill_climbing():
 
                     course_and_activity.remove(keep_track_new_course)
 
-
-                        hour = hour + 1
+                    hour = hour + 1
                 hour = 0
                 day = day + 1
             hour = 0
